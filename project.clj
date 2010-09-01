e4049fb89f7718ae18c9ac9a5c50e2e19b4b9e5e
Mark Gunnels <markgunnels@gmail.com>
2010-08-31 23:59:57 -0400

    fooling around a bit more b/c of the mass quantities of caffeine ingested

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "A library for accessing the Google Social API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-http "0.1.1"]]
+                 [clj-http "0.1.1"]
+                 [org.danlarkin/clojure-json "1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/graphecta/core.clj
+++ src/graphecta/core.clj
@@ -1,11 +1,17 @@
 (ns graphecta.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            (org.danlarkin [json :as json])))
 
 (def *SOCIALGRAPH-BASE-URL* "http://socialgraph.apis.google.com/")
 (def *SOCIALGRAPH-LOOKUP-URL* (str *SOCIALGRAPH-BASE-URL* "lookup"))
-(def *SOCIALGRAPH-OPTIONS* "edo=true&edi=true&fme=true&pretty=true")
+(def *SOCIALGRAPH-OPTIONS* "edo=true&edi=true&fme=true")
 (def *TWITTER-BASE-URL* "http://twitter.com/")
 
 (defn graph-from-twitter
   [twitter-username]
   (client/get (str *SOCIALGRAPH-LOOKUP-URL* "?" *SOCIALGRAPH-OPTIONS* "&q=" *TWITTER-BASE-URL* twitter-username)))
+
+;; this is a play function
+(defn parse-graph
+  [social-graph]
+  (json/decode-from-str (:body social-graph)))
