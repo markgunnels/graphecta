ce1be2b5e5cc758dc301c611784091b7a17688f6
Mark Gunnels <gunnels@musc.edu>
2010-09-02 15:00:56 -0400

    just a bit of work during a conf call

--- src/graphecta/core.clj
+++ src/graphecta/core.clj
@@ -1,5 +1,6 @@
 (ns graphecta.core
-  (:require [clj-http.client :as client]
+  (:require [clojure.contrib.string :as string]
+            [clj-http.client :as client]
             (org.danlarkin [json :as json])))
 
 (def *SOCIALGRAPH-BASE-URL* "http://socialgraph.apis.google.com/")
@@ -7,11 +8,34 @@
 (def *SOCIALGRAPH-OPTIONS* "edo=true&edi=true&fme=true")
 (def *TWITTER-BASE-URL* "http://twitter.com/")
 
+(defn parse-raw-response
+  [raw-response]
+  (json/decode-from-str (:body raw-response)))
+
+(defn extract-twitter-graph
+  [twitter-profile-url social-graph]
+  ((keyword twitter-profile-url) (:nodes social-graph)))
+
 (defn graph-from-twitter
   [twitter-username]
-  (client/get (str *SOCIALGRAPH-LOOKUP-URL* "?" *SOCIALGRAPH-OPTIONS* "&q=" *TWITTER-BASE-URL* twitter-username)))
+  (let [twitter-profile-url (str *TWITTER-BASE-URL* twitter-username)
+        raw-response (client/get (str *SOCIALGRAPH-LOOKUP-URL* "?" *SOCIALGRAPH-OPTIONS* "&q=" twitter-profile-url))
+        social-graph (parse-raw-response raw-response)]
+    (extract-twitter-graph twitter-profile-url social-graph)))
 
-;; this is a play function
-(defn parse-graph
-  [social-graph]
-  (json/decode-from-str (:body social-graph)))
+(defn- element-extractor
+  [twitter-graph key]
+  (map string/as-str (keys (key twitter-graph))))
+
+(defn nodes-referenced-by
+  [twitter-graph]
+  (element-extractor twitter-graph :nodes_referenced_by))
+
+(defn nodes-referenced
+  [twitter-graph]
+  (element-extractor twitter-graph :nodes_referenced))
+
+;(:http://twitter.com/markgunnels (:nodes raw-graph))
+;(:nodes_referenced (:http://twitter.com/markgunnels (:nodes raw-graph)))
+;(:nodes_referenced_by (:http://twitter.com/markgunnels (:nodes raw-graph)))
+
