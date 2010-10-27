b55c22bba0b723209dbf36a7f6d04d61f9934fdf
Mark Gunnels <gunnels@musc.edu>
2010-10-26 20:06:24 -0400

    updated to simplify after CHSRUBY presentation

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.1"]
-                 [org.danlarkin/clojure-json "1.1"]]
+                 [org.danlarkin/clojure-json "1.1"]
+                 [work "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/graphecta/core.clj
+++ src/graphecta/core.clj
@@ -2,7 +2,9 @@
   (:require [clojure.contrib.string :as string]
             [clj-http.client :as client]
             (org.danlarkin [json :as json])
-            [clojure.contrib.seq-utils :as seq-utils]))
+            [clojure.contrib.seq-utils :as seq-utils]
+            [clojure.set :as set]
+            [work.core :as work]))
 
 (def *SOCIALGRAPH-BASE-URL* "http://socialgraph.apis.google.com/")
 (def *SOCIALGRAPH-LOOKUP-URL* (str *SOCIALGRAPH-BASE-URL* "lookup"))
@@ -47,24 +49,9 @@
   [twitter-user-name followers]
   (pmap #(vector % twitter-user-name) followers))
 
-(defn on-node-update
-  [key reference old-state new-state]
-  (pmap #(add-edge-to-social-graph %) new-state))
-
-(defn on-node-error
-  [a error]
-  (println (str a " - " error)))
-
-(defn build-agent
-  [twitter-user-name]
-  (let [a (agent (string/as-str twitter-user-name))]
-    (set-error-handler! a on-node-error)
-    (set-error-mode! a :continue)
-    (add-watch a "graph" on-node-update)))
-
 (defn- element-extractor
   [twitter-graph key]
-  (map #(build-agent %) (keys (key twitter-graph))))
+  (map #(string/as-str %) (keys (key twitter-graph))))
 
 (defn nodes-referenced-by
   [twitter-graph]
@@ -74,25 +61,40 @@
   [twitter-graph]
   (element-extractor twitter-graph :nodes_referenced))
 
-(defn build-social-graph-from-twitter
+(defn build-social-profile-from-twitter
   [twitter-user-name]
   (let [graph (graph-from-twitter twitter-user-name)
         followers (nodes-referenced-by graph)
-        follows (nodes-referenced graph)
-        twitter-user (agent twitter-user-name)]
-    (into (build-followers-graph twitter-user followers)
-          (build-follows-graph twitter-user follows))))
+        follows (nodes-referenced graph)]
+    {:user-name twitter-user-name
+     :followers followers
+     :follows follows}))
 
-(defn build-social-graph
-  [nodes]
-  (doseq [partitioned-nodes (seq-utils/partition-all 5 nodes)]
-    (let [agents (map #(send-off % build-social-graph-from-twitter) partitioned-nodes)]
-      (apply await agents))))
+(defn build-social-graph-from-profile
+  [{:keys [follows followers user-name]}]
+  (into (build-followers-graph user-name followers)
+        (build-follows-graph user-name follows)))
 
-(defn graph-to-search-candidates
-  [edges]
-  (let [candidates (ref #{})]
-    (doseq [edge edges]
-      (dosync
-       (alter candidates into edge)))
-    candidates))
+(defn build-social-graph-from-twitter-user-name
+  [twitter-user-name]
+  (-> twitter-user-name
+      (build-social-profile-from-twitter)
+      (build-social-graph-from-profile)))
+
+(defn build-and-add-social-graph-from-twitter-user-name
+  [twitter-user-name]
+  (-> twitter-user-name
+      (build-social-graph-from-twitter-user-name)
+      (add-nodes-to-social-graph)))
+
+(defn build-candidates-from-profile
+  [{:keys [follows followers user-name]}]
+  (set/union (set follows)
+             (set followers)))
+ 
+(defn populate-social-graph-from-twitter-user-name
+  [twitter-user-name]
+  (let [patient-zero-profile (build-social-profile-from-twitter twitter-user-name)
+        candidates (build-candidates-from-profile patient-zero-profile)]
+    (work/map-work build-and-add-social-graph-from-twitter-user-name (conj candidates twitter-user-name) 2)))
+
